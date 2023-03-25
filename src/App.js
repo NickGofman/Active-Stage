871f74fa0724a6ae775474d02d4a1e7b23eadee7
NickGofman <83462964+NickGofman@users.noreply.github.com>
2023-03-25 23:12:41 +0300

    dark mode with react context, nav bar using assets fileand a constants for the nav link, new logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,54 @@
-import Login from './pages/LoginPage';
+import React from 'react';
+import LoginPage from './pages/LoginPage';
+import Footer from './components/footer/Footer';
+import NavBar from './components/navBar/NavBar';
+
+import DarkModeContext from './DarkModeContext';
+
 function App() {
-  return <Login />;
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  function toggleDarkMode() {
+    setDarkMode(!darkMode);
+  }
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      <div
+        className={
+          darkMode
+            ? 'text-gray-400 bg-gray-900 body-font"'
+            : 'bg-white text-gray-800'
+        }
+      >
+        <header>
+          <label className="flex items-center cursor-pointer">
+            <div className="relative">
+              <input
+                type="checkbox"
+                checked={darkMode}
+                onChange={() => setDarkMode(!darkMode)}
+                className="sr-only"
+              />
+              <div className="block bg-gray-600 w-10 h-4 rounded-full"></div>
+              <div
+                className={`${
+                  darkMode ? 'translate-x-6' : 'translate-x-1'
+                } block absolute inset-y-0 left-0 w-4 h-4 rounded-full bg-white shadow-lg transform transition-transform duration-300`}
+              ></div>
+            </div>
+            <span className="ml-3 text-gray-700 dark:text-white font-medium">
+              Dark Mode
+            </span>
+          </label>
+          <NavBar />
+        </header>
+        <main className="h-screen">
+          <LoginPage />
+        </main>
+        <Footer />
+      </div>
+    </DarkModeContext.Provider>
+  );
 }
 
 export default App;
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import { useContext } from 'react';
+import DarkModeContext from '../../DarkModeContext';
+import ReactLogo from '../../logo/NJs0uK01.svg';
+
+const date = new Date();
+const currentYear = date.getFullYear();
 
 function Footer() {
-  return <div>Footer</div>;
+  const { darkMode } = useContext(DarkModeContext);
+  return (
+    <footer>
+      <div className=" px-5 py-8 mx-auto flex items-center sm:flex-row flex-col justify-center space-y-6">
+        <a className="flex title-font font-medium items-center md:justify-start  text-gray-900">
+          <img
+            src={ReactLogo}
+            alt="Active-Stage Logo"
+            className="w-[124px] h-[80px]"
+          />
+          <span className=" text-gray-600 ml-3 text-xl">Active Stage</span>
+        </a>
+        <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 ">
+          © {currentYear} Active Stage @Nick Gofman & Saar Yankovich
+        </p>
+        <div
+          className="flex
+        "
+        >
+          <a className=" ml-3 text-gray-500">
+            <svg
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-8 h-8"
+              viewBox="0 0 24 24"
+            >
+              <a href="https://www.facebook.com/elis.pub/">
+                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+              </a>
+            </svg>
+          </a>
+
+          <a className="ml-3 text-gray-500">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-8 h-8"
+              viewBox="0 0 24 24"
+            >
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+              <a href="https://www.instagram.com/elispub/?hl=en">
+                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+              </a>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
