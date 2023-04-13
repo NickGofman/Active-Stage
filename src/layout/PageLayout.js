c502e620dcc429b2879553848719249449cb3c16
Saar Yanckovich <saar.yanckovich@gmail.com>
2023-04-13 14:48:08 +0300

    change pageLayout

--- src/layout/PageLayout.js
+++ src/layout/PageLayout.js
@@ -3,17 +3,15 @@ import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
 const PageLayout = ({ children }) => {
   return (
-    <>
-      <div className='flex flex-col h-screen'>
-        <Header />
+    <div className="flex flex-col h-screen">
+      <Header />
 
-        <main className=" flex flex-grow px-5 py-24 mx-auto flex-col justify-center items-center ">
-          {children}
-        </main>
+      <main className=" flex flex-grow px-5 py-24 mx-auto flex-col justify-center items-center ">
+        {children}
+      </main>
 
-        <Footer />
-      </div>
-    </>
+      <Footer />
+    </div>
   );
 };
 export default PageLayout;
