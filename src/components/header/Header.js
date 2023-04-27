a2662cd0ac9a96d3baecb3283c34ad8d02b7eea8
NickGofman <83462964+NickGofman@users.noreply.github.com>
2023-04-27 16:50:30 +0300

    update navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,7 @@ import NavBar from '../navBar/NavBar';
 
 export default function Header() {
   return (
-    <header className="flex mt-4 mr-4 justify-end ">
+    <header className="flex mt-4 mr-4 justify-center">
       <NavBar />
     </header>
   );
--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,60 +1,197 @@
+// import {
+//   Menu,
+//   MenuHandler,
+//   MenuList,
+//   MenuItem,
+//   Avatar,
+//   Typography,
+// } from '@material-tailwind/react';
+// import {
+//   HomeIcon,
+//   PowerIcon,
+//   MusicalNoteIcon,
+//   UserCircleIcon,
+// } from '@heroicons/react/24/outline';
+
+// import { Link } from 'react-router-dom';
+
+// export default function NavBar() {
+//   return (
+//     <Menu>
+//       <MenuHandler>
+//         <Avatar
+//           variant="circular"
+//           alt="candice wu"
+//           className="cursor-pointer"
+//           src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+//         />
+//       </MenuHandler>
+//       <MenuList>
+
+//         <MenuItem className="flex items-center gap-2">
+//           <UserCircleIcon strokeWidth={2} className="h-4 w-4" />
+//           <Typography variant="small" className="font-normal">
+//             My Profile
+//           </Typography>
+//         </MenuItem>
+//         <MenuItem className="flex items-center gap-2">
+//           <HomeIcon strokeWidth={2} className="h-4 w-4" />
+//           <Typography variant="small" className="font-normal">
+//             Home
+//           </Typography>
+//         </MenuItem>
+//         <MenuItem className="flex items-center gap-2">
+//           <MusicalNoteIcon strokeWidth={2} className="h-4 w-4" />
+//           <Typography variant="small" className="font-normal">
+//            My Events
+//           </Typography>
+//         </MenuItem>
+
+//         <hr className="my-2 border-blue-gray-50" />
+//         <MenuItem className="flex items-center gap-2 ">
+//           <PowerIcon strokeWidth={2} className="h-4 w-4" />
+//           <Typography variant="small" className="font-normal">
+//             Sign Out
+//           </Typography>
+//         </MenuItem>
+//       </MenuList>
+//     </Menu>
+//   );
+// }
+import MainLogo from '../../logo/NJs0uK01.svg';
+
+import { useState, useEffect } from 'react';
 import {
-  Menu,
-  MenuHandler,
-  MenuList,
-  MenuItem,
-  Avatar,
+  Navbar,
+  MobileNav,
   Typography,
+  Button,
+  IconButton,
+  Avatar,
 } from '@material-tailwind/react';
-import {
-  HomeIcon,
-  PowerIcon,
-  MusicalNoteIcon,
-  UserCircleIcon,
-} from '@heroicons/react/24/outline';
 
-import { Link } from 'react-router-dom';
+export default function Example() {
+  const [openNav, setOpenNav] = useState(false);
 
-export default function NavBar() {
+  useEffect(() => {
+    window.addEventListener(
+      'resize',
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+
+  const navList = (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Pages
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Account
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Blocks
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Docs
+        </a>
+      </Typography>
+      <Avatar
+        variant="circular"
+        size="lg"
+        alt="candice wu"
+        className="border border-blue-500 p-0.5 hidden lg:block"
+        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+      />
+    </ul>
+  );
+
   return (
-    <Menu>
-      <MenuHandler>
-        <Avatar
-          variant="circular"
-          alt="candice wu"
-          className="cursor-pointer"
-          src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-        />
-      </MenuHandler>
-      <MenuList>
-        
-        <MenuItem className="flex items-center gap-2">
-          <UserCircleIcon strokeWidth={2} className="h-4 w-4" />
-          <Typography variant="small" className="font-normal">
-            My Profile
-          </Typography>
-        </MenuItem>
-        <MenuItem className="flex items-center gap-2">
-          <HomeIcon strokeWidth={2} className="h-4 w-4" />
-          <Typography variant="small" className="font-normal">
-            Home
-          </Typography>
-        </MenuItem>
-        <MenuItem className="flex items-center gap-2">
-          <MusicalNoteIcon strokeWidth={2} className="h-4 w-4" />
-          <Typography variant="small" className="font-normal">
-           My Events
-          </Typography>
-        </MenuItem>
+    <Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4">
+      <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+        <Typography
+          as="a"
+          href="#"
+          variant="small"
+          className="mr-4 cursor-pointer py-1.5 font-normal"
+        >
+          <span>
+            <img
+              src={MainLogo}
+              alt="Active-Stage Logo"
+              className="w-[100px] h-[80px]"
+            />
+          </span>
+        </Typography>
+        <div className="hidden lg:block">{navList}</div>
        
-        <hr className="my-2 border-blue-gray-50" />
-        <MenuItem className="flex items-center gap-2 ">
-          <PowerIcon strokeWidth={2} className="h-4 w-4" />
-          <Typography variant="small" className="font-normal">
-            Sign Out
-          </Typography>
-        </MenuItem>
-      </MenuList>
-    </Menu>
+        <IconButton
+          variant="text"
+          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          ripple={false}
+          onClick={() => setOpenNav(!openNav)}
+        >
+          {openNav ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          )}
+        </IconButton>
+      </div>
+      <MobileNav open={openNav}>
+        <div className="container mx-auto">{navList}</div>
+      </MobileNav>
+    </Navbar>
   );
 }
