b8f10468dccdc85032ecd68b87de7aeb2a1ac52e
Saar Yanckovich <saar.yanckovich@gmail.com>
2023-07-30 13:40:55 +0300

    fix ui bug with the navbar

--- frontEnd/src/layout/PageLayout.js
+++ frontEnd/src/layout/PageLayout.js
@@ -2,21 +2,21 @@ import React, { useContext } from 'react';
 import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
 import { Outlet } from 'react-router-dom';
-import { DarkModeContext } from '../DarkModeContext'; // Update the path to your DarkModeContext file
+import { DarkModeContext } from '../DarkModeContext';
 
 const PageLayout = () => {
   const { darkMode } = useContext(DarkModeContext);
 
   return (
-   
-      <div className="flex flex-col h-screen">
-        <Header darkMode={darkMode} />
-        <main className="flex-1 dark:bg-black dark:text-white">
+    <div className="flex flex-col min-h-screen">
+      <Header darkMode={darkMode} />
+      <main className="flex-1 dark:bg-black dark:text-white">
+        <div className="container mx-auto h-full">
           <Outlet darkMode={darkMode} />
-        </main>
-        <Footer darkMode={darkMode} />
-      </div>
-   
+        </div>
+      </main>
+      <Footer darkMode={darkMode} />
+    </div>
   );
 };
 
